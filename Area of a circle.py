22d6c808e0064275a2c6de9cc9ea66c50af0383a
Sowmya1729 <97968654+Sowmya1729@users.noreply.github.com>
2022-01-29 12:38:13 +0530

    Add files via upload

---
+++ Area of a circle.py
@@ -0,0 +1,11 @@
+π = 3.14  
+r = int (input ("Please enter the radius of the circle: "))  
+area= π * r * r
+print (" The area of the given circle is: ", area )
+
+
+
+
+           
+           
+           
